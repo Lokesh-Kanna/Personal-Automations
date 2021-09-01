74e375bbcb17d1cb68405e5be6463c1418dc7274
Lokesh-Kanna <86400424+Lokesh-Kanna@users.noreply.github.com>
2021-09-01 16:01:20 +0530

    Update GUVI prep.py

--- GUVI prep.py
+++ GUVI prep.py
@@ -4,67 +4,73 @@ import os
 import subprocess
 import pyautogui
 
+previousSession = int(input('What was the previous session number?\n>'))
+
+def waitAndExecute (sec):
+    time.sleep(sec)
+    pyautogui.press('enter')
+
+def wait(sec):
+    time.sleep(sec)
 
 def chooseWindow(x, y):
-    pyautogui.click(151, 1053)        # click on task viwe buton
+    pyautogui.click(151, 1053)                                                # click on task viwe buton
     time.sleep(1)
-    pyautogui.click(x, y)             # click desktop no.
-
-
-
-time.sleep(5)
+    pyautogui.click(x, y)                                                     # click desktop no.
 
 # ------------------------------DESKTOP 3---------------------------------------------------
 
-chooseWindow(555, 91)               # click on desktop 3
+chooseWindow(555, 91)                                                         # click on desktop 3
 time.sleep(2)
-os.system('start OneNote:')          # open onenote
-
+os.system('start OneNote:')                                                   # open onenote
 
 # ------------------------------DESKTOP 1---------------------------------------------------
 
-
-chooseWindow(137, 79)                                        # move to desktop 1
+# Browser Actions
+chooseWindow(137, 79)                                                         # move to desktop 1
 time.sleep(2) 
-webbrowser.open("https://mail.google.com/mail/u/1/#inbox")   # open work mail
-time.sleep(5)
-webbrowser.open("https://www.zenclass.in/class")               # open ZEN class web page
+webbrowser.open("https://www.zenclass.in/class")                              # open ZEN class web page
 time.sleep(8)
-# pyautogui.click(1805, 195)                                         # logout   
-# time.sleep(2)
-# pyautogui.click(1680, 555)                                         # logout   
-# time.sleep(5)
-# pyautogui.click(1626, 213)                                         # login
-# time.sleep(8)
-# pyautogui.click(1270, 802)                                           # signin !!!
-# time.sleep(5)
-pyautogui.click(45, 364)                                     # go to codekata
-time.sleep(3)
-webbrowser.open("https://www.guvi.in/ide")               # open GUVI ide page
-time.sleep(8)
-pyautogui.click(688, 247)                                     # pseudo-click
-time.sleep(1)
-pyautogui.click(688, 247)                                     # Select programming language
-time.sleep(2)
-pyautogui.click(687, 718)                                     # Select JavaScript
-time.sleep(2)
-pyautogui.moveTo(165, 281) 
-time.sleep(1)
-f = open("D://Coding//GUVI//codetemplate.txt", 'r', encoding="utf-8")
-code_template = f.read()                   
-pyautogui.dragTo(739, 944)                                    # select the text
-time.sleep(1)
-pyautogui.write(code_template, interval = 0.10)                                                   # paste the text from onenote
+
+# Cmd actions
+pyautogui.hotkey('ctrl', 'esc')                                               # Open Start menu
+wait(1)
+pyautogui.write('powershell', 0.03)                                           # Search powershell
+waitAndExecute(1)                                                             # Open Windows PowerShell
+wait(4)
+pyautogui.write('D:', 0.02)                                                   # Switch to D drive
+waitAndExecute(0.5)               
+wait(1)
+pyautogui.write('cd Coding/GUVI/"Recorded Sessions"/"Main Bootcamp"', 0.02)   # Cd to Main Bootcamp
+waitAndExecute(0.5)
+wait(2)
+pyautogui.write(f'mkdir "Session {previousSession + 1}"', 0.02)               # Make session folder
+waitAndExecute(0.5)
+wait(1)
+pyautogui.write(f"cd 'Session {previousSession + 1}'")                        # Go to session folder
+waitAndExecute(0.5)
+wait(1)
+pyautogui.write(f'mkdir "Session code"', 0.02)                                # Make code folder
+waitAndExecute(0.5)
+wait(1)
+pyautogui.write("cd 'Session code'")                                          # Go to code folder
+waitAndExecute(0.5)
+wait(1)
+pyautogui.write("New-Item index.html -type file", 0.01)                       # Create index.html file 
+waitAndExecute(0.5)
+wait(1)
+pyautogui.write("New-Item script.js -type file", 0.01)                        # Create script.js file
+waitAndExecute(0.5)
+wait(1)
+pyautogui.write("code .", 0.1)                                                # open code folder in vs code
+waitAndExecute(0.5)
+wait(1)
 
 
-# ------------------------------DESKTOP 2---------------------------------------------------
 
 
-chooseWindow(378, 89)           # move to desktop 2
-# To open an app that is not inbuilt in windows, use subprocess.call('file location')
-subprocess.call('C://Users//iloke//AppData//Roaming//Zoom//bin//Zoom.exe')    # open zoom
 
 
-# ------------------------test---------------------------------------------------------------
+
 # time.sleep(5)
 # print(pyautogui.position())
